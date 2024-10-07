35ad7119af1a14c70d1be89397b5a20dd853b58a
shina369 <flavioalmeidamata@gmail.com>
2024-10-07 23:47:00 +0100

    Improviment in MYSQL_db_tvde_users_external

--- MYSQL_db_tvde_users_external.py
+++ MYSQL_db_tvde_users_external.py
@@ -1,38 +1,46 @@
 import mysql.connector
 
 # Connect to MySQL
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=""  
-)
+def connect():
+    global connection, cursor
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="db_tvde_users_external"
+        
+    )
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
+def commit_and_close_db():
+    connection.commit()
+       
+    cursor.close()
+    connection.close()
+def create_data():
+    # Create database
+    cursor.execute("CREATE DATABASE IF NOT EXISTS db_tvde_users_external")
 
-# Create database
-cursor.execute("CREATE DATABASE IF NOT EXISTS db_tvde_users_external")
+    # Use the new database
+    cursor.execute("USE db_tvde_users_external")
 
-# Use the new database
-cursor.execute("USE db_tvde_users_external")
+    # Create the users table
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_name VARCHAR(255) NOT NULL,
+        user_surname VARCHAR(255) NOT NULL,
+        user_email VARCHAR(255) NOT NULL,
+        user_password VARCHAR(255) NOT NULL,
+        user_profile_photo BLOB
+    )
+    """
+    cursor.execute(create_table_query)
 
-# Create the users table
-create_table_query = """
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL,
-    user_surname VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL,
-    user_profile_photo BLOB
-)
-"""
-cursor.execute(create_table_query)
+    # Commit the transactio
 
-# Commit the transaction
-connection.commit()
+    print("Database and table created successfully.", )
 
-# Close the connection
-cursor.close()
-connection.close()
-
-print("Database and table created successfully.", )
+connect()
+create_data()
+commit_and_close_db()
