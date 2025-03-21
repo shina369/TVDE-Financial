d0183a128e4d9bf0ae5bca72c9526892c88e6ffb
shina369 <flavioalmeidamata@gmail.com>
2025-03-21 14:15:31 +0000

    ...

--- MYSQL_db_tvde_users_external.py
+++ MYSQL_db_tvde_users_external.py
@@ -1,50 +1,45 @@
 import mysql.connector
 import os
 from dotenv import load_dotenv
-
-# Carregar as variáveis do .env
 load_dotenv()
 
-# Obter os dados do ambiente
 MYSQLHOST = os.getenv("MYSQLHOST")
 MYSQLUSER = os.getenv("MYSQLUSER")
 MYSQLPASSWORD = os.getenv("MYSQLPASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-MYSQLPORT = int(os.getenv("MYSQLPORT", 3306))  # Garantir que a porta seja um inteiro
+MYSQLPORT = os.getenv("MYSQLPORT")  # Valor padrão se não estiver definido
 
-# Conectar ao MySQL
+# Connect to MySQL
 def connect():
     global connection, cursor
     connection = mysql.connector.connect(
         host=MYSQLHOST,
         user=MYSQLUSER,
-        password=MYSQLPASSWORD,
-        database=MYSQL_DATABASE,  # Conectar diretamente ao banco de dados
-        port=MYSQLPORT  # Usar a porta configurada
+        password=MYSQLPASSWORD
+,
     )
 
     cursor = connection.cursor()
     if connection.is_connected():
-        print("✅ Conectado ao banco de dados com sucesso!")
-
-# Commit e fechamento da conexão
+        print("Conectado com sucesso!")
+# Commit and Close data
 def commit_and_close_db():
+
     connection.commit()
+       
     cursor.close()
     connection.close()
     if not connection.is_connected():
-        print("❌ Conexão com o banco de dados fechada com sucesso.")
+        print("Conexão com o banco de dados fechada com sucesso.")
 
-# Conectar e criar banco de dados
 connect()
-
-# Criar banco de dados se não existir
+# Create database
 cursor.execute("CREATE DATABASE IF NOT EXISTS db_tvde_users_external")
 
-# Usar o banco de dados criado
+# Use the new database
 cursor.execute("USE db_tvde_users_external")
 
-# Criar a tabela de usuários
+# Create the users table
 create_table_query = """
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -54,11 +49,16 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     date_start VARCHAR(255) NOT NULL
+
 )
 """
 cursor.execute(create_table_query)
 
-# Commit e fechamento
+   # Commit the transactio
+
 commit_and_close_db()
 
-print("✅ Banco de dados e tabela criados com sucesso.")
+print("Database and table created successfully.", )
+
+#connect()
+
