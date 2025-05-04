2099c3c5e6037a6ecc715ba28535d624a264b0c5
shina369 <flavioalmeidamata@gmail.com>
2025-05-04 01:36:16 +0100

    user_id=None in button

--- main.py
+++ main.py
@@ -3827,7 +3827,7 @@ def main(page: ft.Page):
         password_confirm = ft.TextField(label="Password confirm", password=True, can_reveal_password=True, border_radius=21, on_change=validate_password, expand=True)
         
         button_to_db = ft.ElevatedButton(text="REGISTER", bgcolor={"disabled": "#d3d3d3", "": "#4CAF50"}, color="white", disabled=True,
-                                          on_click=lambda e: add_in_db(name.value, surname.value, email.value, password.value))
+                                          on_click=lambda e: add_in_db(name.value, surname.value, email.value, password.value, user_id=None))
         page.views.append(
             ft.View(
                 "/register",
