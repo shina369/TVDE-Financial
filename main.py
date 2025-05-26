b9dcf4f99a3d8a10c029abc86c62c96bafbe4809
shina369 <flavioalmeidamata@gmail.com>
2025-05-26 23:49:29 +0100

    logo small

--- main.py
+++ main.py
@@ -87,8 +87,8 @@ def main(page: ft.Page):
             controls=[
                 ft.Image(
                     src="https://i.ibb.co/FLBSF3xx/Logo-tvde-financial-oficial.png",
-                    width=132,
-                    height=66,
+                    width=120,
+                    height=54,
                 ),
                 pb  # Logo à direita
             ],
